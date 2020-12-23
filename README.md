f3986af0d5a4b6230d2a1a7df3f295b523e71208
Obie <obieewan47@gmail.com>
2020-12-23 13:31:33 +0800

    solved exercise given by KL about multiples

---
+++ README.md
@@ -0,0 +1,21 @@
+# KlMultiple
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `kl_multiple` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:kl_multiple, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/kl_multiple](https://hexdocs.pm/kl_multiple).
+
---
+++ test/kl_multiple_test.exs
@@ -0,0 +1,27 @@
+defmodule KlMultipleTest do
+  use ExUnit.Case
+  doctest KlMultiple
+
+  test "returns {:ok, sum}" do
+    assert KlMultiple.multiple(2,5) ==  {:ok, 30}
+  end
+
+  test "returns error if not number" do
+    assert KlMultiple.multiple("",[]) == :invalid
+  end
+
+  test "returns sum of list" do 
+    assert KlMultiple.sum_multiple([2, 4, 6, 8, 10]) == {:ok, 30}
+  end
+
+  test "returns :not_a_number" do
+    assert KlMultiple.compute_multiple("",[]) == {:error, :not_a_number}
+  end
+
+  test "multiple_calc" do
+    assert KlMultiple.compute_multiple(2,5) == {:ok, [2, 4, 6, 8, 10]}
+  end
+
+
+end
+
