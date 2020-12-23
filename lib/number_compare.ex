574e11b6082fe9f640abd95a8d7eddcacfdaf3fd
Obie <obieewan47@gmail.com>
2020-12-23 20:32:02 +0800

    updated

--- lib/number_compare.ex
+++ lib/number_compare.ex
@@ -2,8 +2,7 @@ defmodule NumberCompare do
   #compares numbers which is greater and returns error
   #if inputs are not numbers
   def greater(number, other_number) do
-    with {:ok, number} <- check_greater(number, other_number),
-         {:ok, number} <- check_greater(number, other_number) do
+    with {:ok, number} <- check_greater(number, other_number) do
       {:ok, number}
     else
       :error  -> {:error, "Invalid Input"}
@@ -24,25 +23,11 @@ defmodule NumberCompare do
     :error
   end
 
-  def check_equals(number, other_number) when is_number(number) and is_number(other_number) do
-    number == other_number
-  end
-
-  def check_equals(_number, _other_number) do
-    :error
-  end
-
     #check boolean returns true or false
-   defp check_bool(number, other_number) when number !== other_number do
-    number > other_number
-  end
-
   defp check_bool(number, other_number) do
-    number == other_number
-    {:true, number} = {true, number}
+    number >= other_number
   end
-
-
+  
 end
 
 
--- test/number_compare_test.exs
+++ test/number_compare_test.exs
@@ -6,7 +6,20 @@ defmodule NumberCompareTest do
     assert NumberCompare.greater(10,5) == {:ok, 10}
   end
 
+  test "check return on 2nd arg greater" do 
+    assert NumberCompare.greater(4,9) == {:ok, 9}
+  end
+
+  test "returns number even equal" do
+    assert NumberCompare.greater(4,4) == {:ok, 4}
+  end
+
   test "check_greater function" do
     assert NumberCompare.check_greater(5,9) == {:ok, 9}
   end
+
+  test "check_greater error return" do
+    assert NumberCompare.check_greater("",[]) == :error
+  end
+
 end
