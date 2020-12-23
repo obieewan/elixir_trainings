0af3fdc612791234123409eca5835f6250cdae6b
Obie <obieewan47@gmail.com>
2020-12-23 15:08:37 +0800

    main/trunk of factorial

---
+++ lib/kl_challenge_factorial.ex
@@ -0,0 +1,18 @@
+defmodule KlChallengeFactorial do
+  @moduledoc """
+  Documentation for `KlChallengeFactorial`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KlChallengeFactorial.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kl_challenge_factorial_test.exs
@@ -0,0 +1,8 @@
+defmodule KlChallengeFactorialTest do
+  use ExUnit.Case
+  doctest KlChallengeFactorial
+
+  test "greets the world" do
+    assert KlChallengeFactorial.hello() == :world
+  end
+end
