a6e0358b296e5bb5ba49d3eb67d5d3fb615b05d8
Obie <obieewan47@gmail.com>
2020-12-23 20:10:31 +0800

    refactoring and still adding test

--- test/number_compare_test.exs
+++ test/number_compare_test.exs
@@ -2,7 +2,11 @@ defmodule NumberCompareTest do
   use ExUnit.Case
   doctest NumberCompare
 
-  test "greets the world" do
-    assert NumberCompare.hello() == :world
+  test "check and returns greater number function caller" do
+    assert NumberCompare.greater(10,5) == {:ok, 10}
+  end
+
+  test "check_greater function" do
+    assert NumberCompare.check_greater(5,9) == {:ok, 9}
   end
 end
