5d56b9a190e711e7b3db9d6dfb1b70a454d2ba06
Obie <obieewan47@gmail.com>
2020-12-24 14:29:59 +0800

    initial running

--- lib/fibonacci_challenge.ex
+++ lib/fibonacci_challenge.ex
@@ -1,18 +1,19 @@
 defmodule FibonacciChallenge do
-  @moduledoc """
-  Documentation for `FibonacciChallenge`.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> FibonacciChallenge.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def calc_fib(0) do
+    0
   end
+
+  def calc_fib(1) do 
+     1
+  end
+
+  def calc_fib(number) do
+    calc_fib(number - 1) + calc_fib(number - 2)
+  end
+
+
+
+
 end
--- test/fibonacci_challenge_test.exs
+++ test/fibonacci_challenge_test.exs
@@ -2,7 +2,13 @@ defmodule FibonacciChallengeTest do
   use ExUnit.Case
   doctest FibonacciChallenge
 
-  test "greets the world" do
-    assert FibonacciChallenge.hello() == :world
+  test "calculate fibonacci n = 10" do
+    assert FibonacciChallenge.calc_fib(10) ==  55
   end
+
+  test "calculate fibonacci n = 5" do
+    assert FibonacciChallenge.calc_fib(5) ==  5
+  end
+
+
 end
