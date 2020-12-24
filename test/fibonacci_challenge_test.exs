8b7a3800495ad418109efae30a46c85b70d5dce7
Obie <obieewan47@gmail.com>
2020-12-24 17:03:07 +0800

    added test

--- test/fibonacci_challenge_test.exs
+++ test/fibonacci_challenge_test.exs
@@ -10,5 +10,13 @@ defmodule FibonacciChallengeTest do
     assert FibonacciChallenge.calc_fib(5) ==  5
   end
 
+  test "calculate fibonnaci n = 20 " do
+    assert FibonacciChallenge.calc_fib(20) == 6765
+  end
+
+  test "calculate fibonacci n = 100" do
+    assert FibonacciChallenge.calc_fib(100) == 354_224_848_179_261_915_075
+  end
+
 
 end
