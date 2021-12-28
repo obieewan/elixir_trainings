d785ef25418a255af92bf8846047200e89f6cfcf
Obie <imo email sa github>
2021-12-29 00:02:17 +0800

    update of supporting codes

---
+++ lib/physics_supporting_codes/rocketry.ex
@@ -0,0 +1,25 @@
+defmodule Physics.Planet do
+
+  defstruct [
+    name: "Earth",    
+    radius_m: 6.371e6,
+    mass_kg: 5.97e24
+
+  ]
+
+
+  def escape_velocity do
+
+  end
+
+
+end
+
+defmodule Physics.Rocketry do
+
+
+  
+
+
+end
+
