ea15e458b96b1f68d4b751ccf861012fb67af6d0
Obie <imo email sa github>
2022-03-19 21:41:45 +0800

    refactor for params

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,14 +2,14 @@ defmodule Dictionary do
 
   def word_list do
     "assets/words.txt"
-    |>File.read!                                      #Reads file from assets
+    |>File.read!()                                      #Reads file from assets
     |>String.split(~r/\n/, trim: true)                # Splits words dictionary inside word.txt excludes empty string
   end
 
 
   def random_word do                                  # Chooses a random word from word list
     word_list
-    |>Enum.random                                     # Picks random word
+    |>Enum.random()                                    # Picks random word
   end
 
  
