3dff34d1a06990b2752b6dfa0dc04c76009d6932
Obie <imo email sa github>
2022-03-19 21:39:25 +0800

    refactored

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,14 +1,15 @@
 defmodule Dictionary do
 
   def word_list do
-    File.read!("assets/words.txt")                    #Reads file from assets
+    "assets/words.txt"
+    |>File.read!                                      #Reads file from assets
     |>String.split(~r/\n/, trim: true)                # Splits words dictionary inside word.txt excludes empty string
   end
 
 
   def random_word do                                  # Chooses a random word from word list
     word_list
-    |>Enum.random
+    |>Enum.random                                     # Picks random word
   end
 
  
