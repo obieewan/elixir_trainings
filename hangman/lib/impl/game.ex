713a57cf2b931afe44b379f3441f1990fba219b9
Obie <imo email sa github>
2022-03-22 21:17:10 +0800

    added specs on new_game which returns type

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,26 +1,29 @@
 defmodule Hangman.Impl.Game do
 
   alias Dictionary, as: Dictionary
+  alias Hangman.Type
 
-  @type t :: %Hangman.Impl.Game{
+  @type t :: %__MODULE__{
     turns_left: integer,
-    game_state: Hangman.state,
-    letters: list(String.t),
-    used: Mapset.t(String.t),
+    game_state: Type.state,
+    letters:    list(String.t),
+    used:       Mapset.t(String.t),
   }
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: [],
-    used: MapSet.new()
+    letters:    [],
+    used:       MapSet.new()
   )
 
+  #spec for new_game returns type of module
+  @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word)
   end
 
+  @spec new_game(String.t) :: t
   def new_game(word) do
-
     %__MODULE__{
       letters: word |> String.codepoints
     }
