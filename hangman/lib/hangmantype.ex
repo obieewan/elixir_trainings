7084ef7e97c226875e5170b52c8a33204b879c48
Obie <imo email sa github>
2022-03-22 21:15:37 +0800

    refactored types from hangman to hangmantypes

---
+++ hangman/lib/hangmantype.ex
@@ -0,0 +1,13 @@
+defmodule Hangman.Type do
+
+  @type   state :: :initiallizing | :won | :lost | :good_guess | :bad_guess | :already_used
+
+
+  @type   tally :: %{
+      turns_left: integer,
+      game_state: state,
+      letters: list(String.t),
+      used: list(String.t)
+    }
+
+end
