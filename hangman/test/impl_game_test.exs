c9478966fbe8fe2fa92993cb44223dadb04ace7f
Obie <imo email sa github>
2022-04-02 16:58:37 +0800

    created a test that doesn't change if a game is won

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -11,13 +11,24 @@ defmodule HangmanImplGameTest do
     assert length(game.letters) > 0
   end
 
-  test "new game returns correct word" do
-    game = Game.new_game()
+
+  test "new game returns correct word in lower case  a to z" do
+    game = Game.new_game("combat")
 
     assert game.turns_left == 7
     assert game.game_state == :initializing
+    assert game.letters    == ["c", "o", "m", "b", "a", "t"]
     assert Enum.all?(game.letters, fn codepoint -> String.match?(codepoint, ~r/[a-z]/) end)
+  end
 
+  test "state doesn't change if a game is won" do
+    game = Game.new_game("combat")
+           |> IO.inspect
+    game = Map.put(game, :game_state, :won)
+           |>IO.inspect
+    { new_game, _tally} = Game.make_move(game, "x")
+
+    assert new_game == game
   end
 
 
