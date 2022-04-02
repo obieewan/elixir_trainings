f4c34015a2a7b14a0abb63577a0f4f9bbcfcca3c
Obie <imo email sa github>
2022-04-03 02:39:25 +0800

    added test that checks duplicate letter and records letter used

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -21,14 +21,33 @@ defmodule HangmanImplGameTest do
     assert Enum.all?(game.letters, fn codepoint -> String.match?(codepoint, ~r/[a-z]/) end)
   end
 
-  test "state doesn't change if a game is won" do
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
     game = Game.new_game("combat")
-           |> IO.inspect
-    game = Map.put(game, :game_state, :won)
-           |>IO.inspect
+    game = Map.put(game, :game_state, state)
     { new_game, _tally} = Game.make_move(game, "x")
 
-    assert new_game == game
+      assert new_game == game  
+    end 
+  end
+
+
+  test " a duplicate letter is reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test " we record letter used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "y")
+    {game, _tally} = Game.make_move(game, "x")
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"])) 
   end
 
 
