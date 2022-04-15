fd97dd90e0b71aa03d2648282aaedac4d36b6ef1
Obie <imo email sa github>
2022-04-15 16:02:09 +0800

    added types in IMPL module and used defdelegate to avoid implementation codes inside API

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,8 +1,10 @@
-defmodule Hangman do
+defmodule Hangman do #API
+
+  alias Hangman.Impl.Game
   
-  @type state:: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
-  @type game :: any
-  @type tally :: %{
+  @type   state:: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @opaque game :: Game.t
+  @type   tally :: %{
     turns_left: integer,
     game_state: state, 
     letters: list(String.t),
@@ -10,8 +12,11 @@ defmodule Hangman do
   }
 
   @spec new_game() :: game 
-  def new_game do
-  end
+  defdelegate new_game, to: Game
+  ##used defdelegate to avoid implementation code in API
+  #def new_game do
+  #  Game.new_game()
+  #end
 
   @spec make_move(game, String.t) :: {game, tally}
   def make_move(game, guess) do
